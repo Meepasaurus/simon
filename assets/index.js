9f270ba3d1f2df3db236e19b67172b544d6c216e
Benjamin Kantor <Meepasaurus@users.noreply.github.com>
2016-08-29 13:10:21 -0400

    start game functionality

--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,7 @@ var Simon = function(turns, score){
 	var maxTurns = turns, //turns before "win" and restart game
 		sequence = [],
 		isStrict = false, //allows retries on incorrect input
+		currentSteps = 0,
 		highScore = score,
 		sfxAlternator = '0'; //helps prevent pops on rapid double-clicks
 
@@ -15,8 +16,15 @@ var Simon = function(turns, score){
 	}
 
 	return {
+		btnClick: function(btnID){
+			this.playSound(btnID);
+			$('.btn-' + btnID).addClass('active-' + btnID);
+			window.setTimeout(function(){
+				$('.btn-' + btnID).removeClass('active-' + btnID);
+			}, 200);
+		},
+
 		playSound: function(soundID){
-			console.log(soundID.toString() + sfxAlternator);
 			var thisBtn = document.getElementById(soundID.toString() + sfxAlternator);
 			thisBtn.pause();
 			thisBtn.currentTime = 0;
@@ -24,13 +32,24 @@ var Simon = function(turns, score){
 			sfxAlternator = (sfxAlternator === '0') ? '1' : '0';
 		},
 
-		generateNext: function(){
-			sequence.push(Math.floor(Math.random() * 4));
-			console.log(sequence);
+		playSequence: function(){
+			for (var i=0, x=sequence.length; i<x; i++){
+				this.btnClick(sequence[i]);
+			}
 		},
 
-		newGame: function(){
+		generateNext: function(){
+			currentSteps++;
+			$('.score').text(currentSteps);
+			sequence.push(Math.floor(Math.random() * 4));
+			console.log(sequence);
+			this.playSequence();
+		},
+
+		newGame: function(strict){
+			isStrict = strict;
 			sequence = [];
+			currentSteps = 0;
 			this.generateNext();
 		}
 	};
@@ -48,13 +67,12 @@ $(document).ready(function(){
 	//recommended 99 turns but freecodecamp asks for 20 turn limit
 	var mySimon = new Simon(20, localScore);
 
-	//document.getElementById('sfx-0').play();
-	//document.getElementById('sfx-1').play();
-	//document.getElementById('sfx-2').play();
-	//document.getElementById('sfx-3').play();
-
 	$('.game-btn').on('click', function(){
-		mySimon.playSound($(this).data('id'));
+		mySimon.btnClick($(this).data('id'));
+	});
+
+	$('#new-game').on('click', function(){
+		mySimon.newGame('false');
 	});
 
 });
